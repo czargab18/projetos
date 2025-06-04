e1e433134c6f6a7edb66be6ebf457c5bba3c5835
cesargabrielphd <87340795+cesargabrielphd@users.noreply.github.com>
2025-06-04 10:25:48 -0300

    cleanup: limpando cache

--- analisedados/captura-cnpj/src/r/unico_source.R
+++
@@ -1,4 +0,0 @@
-# TODOS OS SCRIPT EM UM ÚNICO LUGAR PARA RODAR DE FORMA RÁPIDA
-
-# SETUP PACKAGES ---
-use("dplyr", c("mutate", "case_when", "filter"))
--- frontend/pedrosobral/README.md
+++
@@ -1,3 +0,0 @@
-# Pedro Sobra Laingin Page
-
- Página de Captura **Antiga** das Aulas do Pedro Sobral.
