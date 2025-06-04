e32dd4e48653086f4ac862817554f5f8d95f2601
cesargabrielphd <87340795+cesargabrielphd@users.noreply.github.com>
2025-06-04 09:33:39 -0300

    feat: adicionar função de tema personalizado para gráficos e atualizar visualização de dados

--- analisedados/captura-cnpj/src/r/manipul/graficos.R
+++ analisedados/captura-cnpj/src/r/manipul/graficos.R
@@ -5,7 +5,29 @@ library(dplyr)
 library(ggplot2)
 library(dplyr)
 
-plot_tipo <-
+# FUNCTIONS ----
+theme_custom <- function(plot, title, title.y) {
+  plot +
+    labs(
+      title = title,
+      y = title.y
+    ) +
+    theme(
+      axis.title.y = element_blank(),
+      panel.grid.major = element_blank(),
+      panel.grid.major.x = element_line(color = "#c0bebe", linetype = "dashed"),
+      panel.grid.minor = element_blank(),
+      panel.background = element_blank(),
+      axis.ticks.y = element_blank(),
+      axis.line.x.bottom = element_line(color = "black")
+    )
+}
+
+# GRÁFICOS ----
+
+names(base_mcti_request)
+
+plot_tipo_dispendio <-
 base_mcti_request |>
   dplyr::count(tipo_dispendio) |>
   dplyr::mutate(tipo_dispendio = factor(tipo_dispendio, levels = tipo_dispendio[order(n)])) |>
@@ -13,21 +35,22 @@ base_mcti_request |>
   geom_bar(stat = "identity") +
   coord_flip()
 
+theme_custom(
+  plot_tipo_dispendio,
+  title = "Tipos de Parcerias em projetos de P,D&I",
+  title.y = "Quantidade de Projetos"
+)
 
-plot_tipo +
-    labs(
-      title = "Tipos de Parcerias em projetos de P,D&I",
-      y = "Quantidade de Projetos"
-    ) +
-  theme(
-    axis.title.y = element_blank(),
-    panel.grid.major = element_blank(),
-    panel.grid.major.x = element_line(color = "#c0bebe", linetype = "dashed"),
-    panel.grid.minor = element_blank(),
-    panel.background = element_blank(),
-    axis.ticks.y = element_blank(),
-    axis.line.x.bottom = element_line(color = "black")
-  )
+uf_dispendio <-
+base_mcti_request |>
+  dplyr::count(uf.y) |>
+  dplyr::mutate(uf.y = factor(uf.y, levels = uf.y[order(n)])) |>
+  ggplot(aes(x = uf.y, y = n)) +
+  geom_bar(stat = "identity") +
+  coord_flip()
 
-
-names(base_mcti_request)
+theme_custom(
+  plot_tipo,
+  title = "Tipos de Parcerias em projetos de P,D&I",
+  title.y = "Quantidade de Projetos"
+)
