437ed8022d65de2921fd94e59e868360c7f77dce
cesargabrielphd <87340795+cesargabrielphd@users.noreply.github.com>
2025-06-05 08:51:21 -0300

    feat: adicionar limpeza e formatação de campos no script de leitura da base MCTI

--- analisedados/captura-cnpj/src/r/read/read_request.R
+++ analisedados/captura-cnpj/src/r/read/read_request.R
@@ -7,4 +7,38 @@ base_request <- read_delim(
   col_types = list(col_character()),
   delim = ";"
 ) |>
-janitor::clean_names()
+  janitor::clean_names()  |>
+  dplyr::mutate(
+    dplyr::across(
+      c(
+        nome_fantasia,
+        razao_social,
+        descricao_cnae_fiscal,
+        descricao_tipo_de_logradouro,
+        logradouro,
+        complemento,
+        bairro,
+        municipio,
+        nome_cidade_no_exterior,
+        qsa,
+        cna_es_secundarios
+      ),
+      ~ stringr::str_replace_all(.x, "\\s*-\\s*", " ") |>
+        stringr::str_replace_all("\\s*[\\-–]\\s*", ". ") |>
+        stringr::str_replace_all(";\\s*", ". ")
+    )
+  )
+
+view(base_request)
+
+# SALVANDO BASE COMPLETA ----
+# usar separador ";"
+write.table(
+  x = base_request,
+  file = "data/processed/base_request.csv",
+  sep = ";",
+  row.names = FALSE,
+  col.names = TRUE,
+  quote = TRUE,
+  fileEncoding = "UTF-8"
+)
