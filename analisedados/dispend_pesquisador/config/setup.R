761ddfc4e831e1515097697a7429b26e731dfb07
cesargabrielphd <87340795+cesargabrielphd@users.noreply.github.com>
2025-06-12 10:04:39 -0300

    feat: adicionar configuração do renv e atualizar .gitignore para gerenciamento de pacotes

---
+++ analisedados/dispend_pesquisador/config/setup.R
@@ -0,0 +1,37 @@
+# SETUP PROJECT ----
+options(
+  stringsAsFactors = FALSE,
+  repos = c(CRAN = "https://vps.fmvz.usp.br/CRAN/")
+)
+
+# SETUP RENV ----
+if (!require("renv", quietly = TRUE)) {
+  install.packages("renv")
+} else {
+  print("iniciando o RENV")
+  renv::init()
+  #> 3: Activate the project
+}
+# ativar caso já exista
+renv::activate()
+
+
+# SETUP DIRETÓRIOS ----
+DIR_PROJECT <- getwd()
+# [1] "C:/Users/cesar.oliveira/github/projetos/analisedados/dispend_pesquisador"
+
+# bibliotecas em renv/*
+DIR_PACKAGES <- .libPaths(renv::paths$library())
+
+
+# SETUP PACKAGES ----
+PACKAGES <- c("dplyr", "ggplot2", "readr", "readxl", "fs", "janitor", "writexl", "purrr")
+
+# Instala e carrega os pacotes usando {renv}
+for (pkg in PACKAGES) {
+  if (!requireNamespace(pkg, quietly = TRUE)) {
+    install.packages(pkg)
+    renv::snapshot(prompt = TRUE)
+  }
+  library(pkg, character.only = TRUE)
+}
