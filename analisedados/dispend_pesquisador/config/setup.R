842026861825ca4104265d687d773a283f70f423
cesargabrielphd <87340795+cesargabrielphd@users.noreply.github.com>
2025-06-13 10:40:51 -0300

    feat: reorganizar a lista de pacotes e atualizar a instalação e snapshot do renv

--- analisedados/dispend_pesquisador/config/setup.R
+++ analisedados/dispend_pesquisador/config/setup.R
@@ -25,13 +25,25 @@ DIR_PACKAGES <- .libPaths(renv::paths$library())
 
 
 # SETUP PACKAGES ----
-PACKAGES <- c("dplyr", "ggplot2", "readr", "readxl", "fs", "janitor", "writexl", "purrr", "stringi")
+PACKAGES <- c(
+  "dplyr",
+  "ggplot2",
+  "readr",
+  "readxl",
+  "fs",
+  "janitor",
+  "writexl",
+  "purrr",
+  "stringi"
+)
 
 # Instala e carrega os pacotes usando {renv}
 for (pkg in PACKAGES) {
   if (!requireNamespace(pkg, quietly = TRUE)) {
-    install.packages(pkg)
-    renv::snapshot(prompt = TRUE)
+    renv::install(pkg)
   }
   library(pkg, character.only = TRUE)
 }
+
+# faz snapshot após instalar/tocar todos os pacotes
+renv::snapshot(prompt = FALSE)
