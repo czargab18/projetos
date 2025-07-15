3c1f368330ad295f3272a427ba3e2d3c45dfdf96
czargab18 <87340795+czargab18@users.noreply.github.com>
2025-07-15 08:33:12 -0300

    refactor: reorganizar seções de configuração e carregamento de dados no script de gráficos

--- analisedados/captura-cnpj/src/r/graficos/build.grafico.R
+++ analisedados/captura-cnpj/src/r/graficos/build.grafico.R
@@ -1,3 +1,9 @@
-# TEMAS ----
+# CONFIGURAÇÕES INICIAIS ----
+#> configuração do projeto, pacotes e diretórios
+source("config/setup.R")
+#> carregar e unir as bases de dados
+source("src/r/read/read_and_join_bases.R")
+#> carregar funções auxiliares para gráficos
 source("src/r/graficos/temas.R")
 
+# ----
