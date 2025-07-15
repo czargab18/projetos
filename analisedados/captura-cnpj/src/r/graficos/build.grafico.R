719184d401fd2307c6f85aa3b27dbe092fb96b2c
czargab18 <87340795+czargab18@users.noreply.github.com>
2025-07-15 09:28:30 -0300

    refactor: otimizar a criação do gráfico de tipos de parcerias e corrigir variáveis obsoletas

--- analisedados/captura-cnpj/src/r/graficos/build.grafico.R
+++ analisedados/captura-cnpj/src/r/graficos/build.grafico.R
@@ -7,26 +7,41 @@ source("src/r/read/read_and_join_bases.R")
 source("src/r/graficos/temas.R")
 
 # GRÁFICO: TIPOS DE PARCERIAS ----
+base_mcti_request |>
+  select(tipo_dispendio, cnpj_dispendio) |>
+  group_by(tipo_dispendio) |>
+  summarise(cnpjs = list(cnpj_dispendio)) |>
+  mutate(
+    cnpjs = map(cnpjs, unique),
+    qtd_cnpjs = lengths(cnpjs)
+  )
 
-#plot_tipo_dispendio <-
-  base_mcti_request |>
+
+# plot_tipo_dispendio <-
+base_mcti_request |>
   dplyr::select(tipo_dispendio, cnpj_dispendio) |>
-
-  dplyr::count(tipo_dispendio) |>
+  dplyr::group_by(tipo_dispendio) |>
+  dplyr::summarise(cnpj_dispendio = list(cnpj_dispendio)) |>
   dplyr::mutate(
+    cnpj_dispendio = map(cnpj_dispendio, unique),
+    quantidade = lengths(cnpj_dispendio),
     tipo_dispendio = factor(
       tipo_dispendio,
-      levels = tipo_dispendio[order(n)]
+      levels = tipo_dispendio[order(quantidade)]
     )
   ) |>
-  ggplot(aes(x = tipo_dispendio, y = n)) +
+  dplyr::select(tipo_dispendio, quantidade) |>
+  dplyr::arrange(desc(quantidade)) |>
+  ggplot(aes(x = tipo_dispendio, y = quantidade)) +
   geom_bar(stat = "identity") +
   coord_flip() +
   labs(
     x = "Tipos de Parcerias",
     y = "Quantidade de Projetos"
   ) +
-  ggplot2::geom_text(aes(label = n), color = "#000000", size = 4) +
+  ggplot2::geom_text(aes(label = quantidade), color = "#000000", size = 4) +
   tema()
+
+
 
 # GRÁFICO:
